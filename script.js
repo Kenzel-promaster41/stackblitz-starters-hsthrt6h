61db26ea6ab82e4cb9948d4a638d122a7958d864
Kenzel-promaster41 <lantanokenzel@icloud.com>
2025-12-17 18:49:28 -0500

    final changes to website maybe

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello!');
+function submitForm() {
+    var userName = document.getElementById("username").value;
+    var gameType = document.getElementById("gameType").value;
+    var subscribed = document.getElementById("subscribe").checked;
+  
+    if (userName === "") {
+      alert("Please enter your name.");
+    } else {
+      var message = "Name: " + userName +
+        "\nFavorite Game Type: " + gameType;
+  
+      if (subscribed) {
+        message += "\nSubscribed: Yes";
+      } else {
+        message += "\nSubscribed: No";
+      }
+  
+      alert(message);
+    }
+  }
+  
